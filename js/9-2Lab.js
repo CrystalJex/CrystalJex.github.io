fbf95996c91f850fdf25a34474eb5e50ca8978ab
CrystalJex.github.io <cjex2008@gmail.com>
2019-06-24 14:38:26 -0600

    a

--- js/9-2Lab.js
+++ js/9-2Lab.js
@@ -7,13 +7,14 @@ Output: Output the temple associated with the lowest number.
 let table = document.getElementById("templeData");
 let year = parseInt(table.rows[1].cells[1].innerHTML);
 let rows = document.getElementById("templeData").rows.length;
-let oldestTemple = table.rows[i].cells[0].innerHTML;
+
 
 //Processing
 for (let i=1; i<=rows; i++) {
 	let compareYear = parseInt(table.rows[i].cells[1].innerHTML);
 	if (year <= compareYear) {
 		year = parseInt(table.rows[i].cells[1].innerHTML);
+		let oldestTemple = table.rows[i].cells[0].innerHTML;
 		
 		//Output
 		document.getElementById("output").innerHTML = oldestTemple;
